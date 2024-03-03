5cbc7401748c3ec0c91923321ae6ed2750ee255c
Ivan Leonardi <ivanthaisjesus@gmail.com>
2024-03-02 22:19:22 -0300

    add task item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,10 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import Tasks from './components/Tasks';
 
-import TaskItem from "./components/TaskItem";
-
-const App = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: "1",
-            description: "Estudar programação",
-            isCompleted: false,
-        },
-        {
-            id: "2",
-            description: "Estudar Banco de Dados",
-            isCompleted: true,
-        },
-    ]);
-
-    const fetchTasks = async () => {
-        try {
-            const { data } = await axios.get("http://localhost:8000/tasks");
-
-            setTasks(data);
-
-            console.log(response);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchTasks();
-    }, []);
+const App = () => {   
 
     return (
         <div>
-            {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
-            ))}
+           <Tasks />
         </div>
     );
 };
