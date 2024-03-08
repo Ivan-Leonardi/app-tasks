95fec1eb18557fcf16cf3d42e4881b3d80cd8c3f
Ivan Leonardi <ivanthaisjesus@gmail.com>
2024-03-08 01:04:08 -0300

    feat: add Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,20 @@
+import CustomButton from "../components/CustomButton";
+
+import Logo from "../assets/images/google-forms.png";
+
+import "./Login.scss";
+
 const Login = () => {
     return (
-        <div></div>
+       <div className="login-container">
+        <img src={Logo} alt="logo do app" />
+        <div className="button-container">
+            <CustomButton>
+                Entrar
+            </CustomButton>
+        </div>
+       </div>
+
     )
 }
 
