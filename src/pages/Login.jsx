de881ffcc0eaf1e377f1b12e9dbe8a5ad455dda2
Ivan Leonardi <ivanthaisjesus@gmail.com>
2024-03-21 23:17:51 -0300

    feat: add navigation to home page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import CustomButton from "../components/CustomButton";
 
 import Logo from "../assets/images/google-forms.png";
@@ -5,12 +7,20 @@ import Logo from "../assets/images/google-forms.png";
 import "./Login.scss";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate("/");
+    };
+
     return (
         <div className="login-container">
             <img src={Logo} alt="logo do app" />
-            <div><h2>App Tarefas</h2></div>
+            <div>
+                <h2>Gerenciador Tarefas</h2>
+            </div>
             <div className="button-container">
-                <CustomButton>Entrar</CustomButton>
+                <CustomButton onClick={handleSignInClick}>Entrar</CustomButton>
             </div>
         </div>
     );
